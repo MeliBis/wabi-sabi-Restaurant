07cf06d4b45e44ce187f8732d480733e8df48c43
Meli Barrios <melissa.blecona@gmail.com>
2023-11-21 10:19:49 -0600

    primera prueba de home page terminada

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import NavBar from '../components/generales/NavBar'
+import Title from '../components/home/Title'
+import Cards from '../components/home/Cards'
+import Anuncio2 from '../components/home/Anuncio2'
+import Slide from '../components/home/Slide'
+import Footer from '../components/generales/Footer'
 
 const HomePage = () => {
   return (
     <>
     <NavBar/>
-        
+    <Title/>  
+    <Cards/>
+    <Anuncio2/>
+    <Slide/>
+    <Footer/>
     </>
   )
 }
